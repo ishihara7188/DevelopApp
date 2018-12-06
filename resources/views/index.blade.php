bf580f2133b8718741490097ee31adc553e973dd
totti <r.funk.7188@gmail.com>
2018-12-06 20:52:27 +0900

    first commit add auth navbar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.top')
+@section('title', 'Scheduler')
+@section('content')
+    <h1>Scheduler</h1>
+    <p>Make your schedule.</p>
+
+
+@endsection
---
+++ resources/views/layouts/top.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+      <script src="{{ asset('js/app.js') }}" defer></script>
+       <!-- Fonts -->
+       <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+       <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+       <!-- Styles -->
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+       <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+       <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+  </head>
+  <body>
+      @include('partials.header')
+      <div class="container">
+          @yield('content')
+      </div>
+  </body>
+</html>
